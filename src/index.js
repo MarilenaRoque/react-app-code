8b245a08b98ad9c0c22a3797fe1c302b767d11bd
MarilenaRoque <marilenas@id.uff.br>
2020-12-06 14:09:49 -0300

    Adding event handler

--- src/index.js
+++ src/index.js
@@ -5,36 +5,42 @@ import ReactDom from "react-dom";
 import "./index.css";
 const books = [
   {
+    id: 1,
     title: "The Deep End (Diary of a Wimpy Kid Book 15)",
     author: "Jeff Kinney",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/71Ks%2B8mKq2L._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 2,
     title: "Cat Kid Comic Club: From the Creator of Dog Man",
     author: "Dav Pilkey",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/810Ukmf7bSL._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 3,
     title: "If Animals Kissed Good Night",
     author: "Ann Whitford Paul",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/817T4J3dzhL._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 4,
     title: "How the Grinch Stole Christmas!",
     author: "Dr. Seuss",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/91Q9eneR7BL._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 5,
     title: "Dog Man: Grime and Punishment",
     author: "Dav Pilkey",
     image:
       "https://images-na.ssl-images-amazon.com/images/I/71c1LRLBTBL._AC_UL200_SR200,200_.jpg",
   },
   {
+    id: 6,
     title: "The Boy, the Mole, the Fox and the Horse",
     author: "Charlie Mackesy",
     image:
@@ -46,9 +52,9 @@ const BookList = () => {
   return (
     <section className="bookList">
         {books.map((book)=> {
-          const {image, author, title} = book;
+          const {image, author, title, id} = book;
           return(
-           <Book book={book}></Book>
+           <Book key={id} {...book}></Book>
           )
         })}
     </section>
@@ -56,11 +62,14 @@ const BookList = () => {
 };
 
 const Book = (props) => {
-  const { title, image, author, children} = props.book;
+  const { title, image, author, children} = props;
+  const clickHandler = () => {
+    alert("Don't Click!");
+  }
   return (
     <article className="book">
       <img alt="The Deep End" src={image} height="200" width="200"></img>
-      <h1>{title}</h1>
+      <h1 onClick={clickHandler}>{title}</h1>
       {children}
       <h4 style={{ color: "#617d98", fontSize: "0.75rem" }}>{author}</h4>
     </article>
